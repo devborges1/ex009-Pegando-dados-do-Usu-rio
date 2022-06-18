703824a00c9b2ec522d53d24c3315696c3b532be
Roberto Borges <aqueleroberto1@protonmail.com>
2022-06-18 12:50:58 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+alert('Bem-vindo! A seguir pediremos que informe alguns dados.')
+
+let user = prompt('Qual o seu nome?')
+
+let age = prompt('Qual sua idade?')
+
+let ageConfirm = confirm('Sua idade é ' + age + ' anos')
+
+alert('Nome: ' + user + '\nIdade: ' + age + '\nIdade Confirmada: ' + ageConfirm)
